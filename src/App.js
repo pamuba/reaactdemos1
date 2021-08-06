f7f0b4124761a76bc7300a47eef7b07817b3738e
pamuba <partha.bora24@gmail.com>
2021-08-06 21:24:55 +0530

    311

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import HookCounter from './components/HookCounter';
+import HookCounterTwo from './components/HookCounterTwo';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HookCounter></HookCounter>
+        <HookCounterTwo></HookCounterTwo>
       </header>
     </div>
   );
